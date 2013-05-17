3a1d51bfb38d2c21c262a1b66cec14e960b48c6d
Dave Yeo <daveryeo@telus.net>
2013-05-16 21:28:41 -0700

    Add define FT_CONFIG_OPTION_OLD_INTERNALS to fix build, this needs to be removed next version

--- freetype/builds/os2/os2-gcc.mk
+++ freetype/builds/os2/os2-gcc.mk
@@ -24,7 +24,8 @@ include $(TOP_DIR)/builds/os2/os2-def.mk
 
 # include gcc-specific definitions
 include $(TOP_DIR)/builds/compiler/gcc.mk
-CFLAGS +=-D__EMX__ -DOS2  -D__OS2__ -D__ST_MT_ERRNO__ -march=pentium -mtune=pentium4
+# note the -DFT_CONFIG_OPTION_OLD_INTERNALS is a temporary fix to be removed after 2.1.12
+CFLAGS +=-DFT_CONFIG_OPTION_OLD_INTERNALS -D__EMX__ -DOS2  -D__OS2__ -D__ST_MT_ERRNO__ -march=pentium -mtune=pentium4
 
 # include linking instructions
 ifeq ($(DLL),yes)
